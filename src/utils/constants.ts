9b4b98c507b5b308f5a12036cd97c625166b37a0
VictorH74 <victorh.almeida7@gmail.com>
2023-09-15 09:14:34 -0300

    fix: build error

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const TOKEN = process.env.NEXT_PUBLIC_GITHUB_TOKEN || (() => {throw new Error("NEXT_PUBLIC_GITHUB_TOKEN not defined")})()
+
 
 export const THEME_COLOR_KEY = "vh_portfolio-theme_color"
