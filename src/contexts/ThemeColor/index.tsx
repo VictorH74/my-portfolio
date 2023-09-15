8cdc4c2289285617e84573ba169a3edd8e29621a
VictorH74 <victorh.almeida7@gmail.com>
2023-09-15 09:12:18 -0300

    extra

--- src/contexts/ThemeColor/index.tsx
+++ src/contexts/ThemeColor/index.tsx
@@ -1,3 +1,4 @@
+import { THEME_COLOR_KEY } from "@/utils/constants";
 import React from "react";
 
 interface AppContextInterface {
@@ -17,12 +18,22 @@ export const ThemeContext = React.createContext<AppContextInterface>({
 export const ThemeProvider: React.FC<Props> = ({ children }) => {
   const [themeColor, setThemeColorState] = React.useState("#4e54fd");
 
-  const setThemeColor = (color: string) => setThemeColorState(color);
+  const setThemeColor = (color: string) => {
+    localStorage.setItem(THEME_COLOR_KEY, color)
+    setThemeColorState(color);
+  };
 
   const values: AppContextInterface = {
     themeColor,
     setThemeColor,
   };
+
+  React.useEffect(() => {
+    const recoveredColor = localStorage.getItem(THEME_COLOR_KEY);
+    if (recoveredColor === null) return;
+
+    setThemeColorState(recoveredColor);
+  }, []);
 
   return (
     <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
