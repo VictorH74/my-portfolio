cf185ac73396817fe1a441d2595c50459494f301
VictorH74 <victorh.almeida7@gmail.com>
2024-04-04 00:03:44 -0300

    extra

--- src/app/api/technologies/route.ts
+++ src/app/api/technologies/route.ts
@@ -1,13 +1,18 @@
 import { TechIcons } from "@/types";
 
-export const GIST_URL = process.env.GIST_URL || 
+export const GIST_ID = process.env.GIST_ID || 
+ (() => { throw new Error("No Gist Id provided") })()
+
+ export const GIST_URL = process.env.GIST_URL || 
  (() => { throw new Error("No Gist URL provided") })()
 
 export const GITHUB_GIST_TOKEN = process.env.GITHUB_GIST_TOKEN ||
  (() => { throw new Error("No Gist Token provided") })()
 
+const gistUrl = `${GIST_URL}/${GIST_ID}`
+
 export async function GET(req: Request) {
-    const res = await fetch(GIST_URL);
+    const res = await fetch(gistUrl);
     const gist = await res.json()
 
     if (!gist.files) return Response.json({ "message": "error" });
@@ -17,14 +22,14 @@ export async function GET(req: Request) {
 }
 
 export async function POST(req: Request) {
-    const res = await fetch(GIST_URL);
+    const res = await fetch(gistUrl);
     const data = await res.json()
 
     if (!data.files) return Response.error();
 
     const contentData = req.body as any;
 
-    await fetch(GIST_URL, {
+    await fetch(gistUrl, {
         method: "PATCH",
         headers: {
             Authorization: `token ${GITHUB_GIST_TOKEN}`,
