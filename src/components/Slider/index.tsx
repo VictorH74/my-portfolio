37b740d56227b543b70d826c1e41f3d471666fae
VictorH74 <victorh.almeida7@gmail.com>
2025-02-13 15:31:32 -0300

    feat: Slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable jsx-a11y/alt-text */
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import Image, { ImageProps } from 'next/image';
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import { useSlider, SliderProps } from './useSlider';
+
+export const Slider = (props: SliderProps) => {
+    const hook = useSlider(props);
+
+    return (
+        <div
+            className="relative h-full"
+            onTouchStart={hook.handleTouchStart}
+            onTouchMove={hook.handleTouchMove}
+            onTouchEnd={hook.handleTouchEnd}
+            onMouseLeave={hook.handleMouseUp}
+            onMouseDown={hook.handleMouseDown}
+            onMouseMove={hook.handleMouseMove}
+            onMouseUp={hook.handleMouseUp}
+        >
+            <ul
+                ref={hook.sliderContainerRef}
+                className="flex w-full overflow-hidden group/container h-full"
+            >
+                {props.images.map((file, index) => {
+                    return (
+                        <li key={index} className="w-full flex-shrink-0">
+                            <SlideImage
+                                width={780}
+                                height={300}
+                                src={file.url}
+                                alt={file.name}
+                            />
+                        </li>
+                    );
+                })}
+                {props.images.length > 1 && (
+                    <span className="absolute bottom-0 py-2 inset-x-0 flex justify-center">
+                        <div className="absolute inset-0 z-0 bg-gradient-to-t from-black/50 duration-200 group-hover/container:opacity-100 opacity-0 max-lg:hidden" />
+                        {props.images.map((_, i) => (
+                            <button
+                                key={i}
+                                className="p-1 group/btn z-10"
+                                onClick={() => hook.changeCurrentFileIndex(i)}
+                                type="button"
+                            >
+                                <div
+                                    className={twMerge(
+                                        'size-3 rounded-full duration-200 group-hover/btn:scale-150 bg-[#d6d6d6]',
+                                        hook.currentFileIndex == i &&
+                                            'bg-orange-500'
+                                    )}
+                                />
+                            </button>
+                        ))}
+                    </span>
+                )}
+                {props.images.length > 1 &&
+                    [
+                        {
+                            label: 'prev',
+                            Icon: NavigateBeforeIcon,
+                            onClick: hook.previousSlide,
+                            className: 'left-0',
+                        },
+                        {
+                            label: 'next',
+                            Icon: NavigateNextIcon,
+                            onClick: hook.nextSlide,
+                            className: 'right-0',
+                        },
+                    ].map((btn) => (
+                        <button
+                            key={btn.label}
+                            onClick={btn.onClick}
+                            className={twMerge(
+                                'absolute inset-y-0 p-4 opacity-0 max-lg:opacity-100 group-hover/container:opacity-100 duration-200 lg:hover:scale-150 text-orange-500',
+                                btn.className
+                            )}
+                        >
+                            <btn.Icon sx={{ fontSize: 40 }} />
+                        </button>
+                    ))}
+            </ul>
+        </div>
+    );
+};
+
+const SlideImage: React.FC<
+    Omit<ImageProps, 'className' | 'onLoad' | 'loading'>
+> = (props) => {
+    const [loadingImg, setLoadingImg] = React.useState(true);
+
+    return (
+        <div className="size-full relative select-none">
+            <Image
+                {...props}
+                className="h-full w-auto object-contain"
+                onLoad={() => setLoadingImg(false)}
+                loading="lazy"
+            />
+            <div
+                className={`absolute object-cover inset-0 bg-transparent grid place-items-center ${
+                    loadingImg ? 'opacity-100' : 'opacity-0'
+                }`}
+            >
+                {/* <Loading /> */}
+            </div>
+        </div>
+    );
+};
---
+++ src/components/Slider/useSlider.tsx
@@ -0,0 +1,115 @@
+import { ScreenshotType } from '@/types';
+import React from 'react';
+
+export interface SliderProps {
+    images: ScreenshotType[];
+}
+
+type PositionType = {
+    x: number;
+    y: number;
+};
+
+export const useSlider = (props: SliderProps) => {
+    const sliderContainerRef = React.useRef<HTMLUListElement>(null);
+
+    const [currentFileIndex, setCurrentFileIndex] = React.useState<number>(0);
+
+    const [startPos, setStartPos] = React.useState<PositionType | null>(null);
+    const [isDragging, setIsDragging] = React.useState(false);
+
+    React.useEffect(() => {
+        if (!sliderContainerRef.current) return;
+
+        const sliderContainer = sliderContainerRef.current;
+        const left = sliderContainer.clientWidth * currentFileIndex;
+
+        sliderContainer.scrollTo({ left, behavior: 'smooth' });
+    }, [currentFileIndex]);
+
+    const changeCurrentFileIndex = (index: number) =>
+        setCurrentFileIndex(index);
+
+    const previousSlide = React.useCallback(() => {
+        if (currentFileIndex == 0)
+            return setCurrentFileIndex(props.images.length - 1);
+        setCurrentFileIndex((prev) => prev - 1);
+    }, [currentFileIndex, props.images.length]);
+
+    const nextSlide = React.useCallback(() => {
+        if (currentFileIndex + 1 >= props.images.length)
+            return setCurrentFileIndex(0);
+        setCurrentFileIndex((prev) => prev + 1);
+    }, [currentFileIndex, props.images]);
+
+    const handleTouchStart = (e: React.TouchEvent) => {
+        const { clientX, clientY } = e.touches[0];
+        setStartPos({ x: clientX, y: clientY });
+        setIsDragging(true);
+    };
+
+    const handleTouchMove = (e: React.TouchEvent) => {
+        if (!isDragging || startPos === null) return;
+        const currentXPos = e.touches[0].clientX;
+        const currentYPos = e.touches[0].clientY;
+        const xDiff = startPos.x - currentXPos;
+        const yDiff = startPos.y - currentYPos;
+
+        const longYMoviment = yDiff < 30 && yDiff > -30;
+        if (longYMoviment && xDiff > 50) {
+            nextSlide();
+            setIsDragging(false);
+        } else if (longYMoviment && xDiff < -50) {
+            previousSlide();
+            setIsDragging(false);
+        }
+    };
+
+    const handleTouchEnd = () => {
+        setIsDragging(false);
+        setStartPos(null);
+    };
+
+    const handleMouseDown = (e: React.MouseEvent) => {
+        const { clientX, clientY } = e;
+        setStartPos({ x: clientX, y: clientY });
+        setIsDragging(true);
+    };
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        if (!isDragging || startPos === null) return;
+        const xDiff = startPos.x - e.clientX;
+        const yDiff = startPos.y - e.clientY;
+
+        const longYMoviment = yDiff < 30 && yDiff > -30;
+
+        if (longYMoviment && xDiff > 50) {
+            nextSlide();
+            setIsDragging(false);
+        } else if (longYMoviment && xDiff < -50) {
+            previousSlide();
+            setIsDragging(false);
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+        setStartPos(null);
+    };
+
+    return {
+        sliderContainerRef,
+        currentFileIndex,
+        startPos,
+        isDragging,
+        changeCurrentFileIndex,
+        previousSlide,
+        nextSlide,
+        handleTouchStart,
+        handleTouchMove,
+        handleTouchEnd,
+        handleMouseDown,
+        handleMouseMove,
+        handleMouseUp,
+    };
+};
